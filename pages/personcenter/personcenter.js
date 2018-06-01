a0a36ae3984f2489efe312f23e52aedd87eb62cd
xr <xr@wolvestribe.com>
2018-06-01 19:27:42 +0800

    modify

--- pages/personcenter/personcenter.js
+++ pages/personcenter/personcenter.js
@@ -1,15 +1,72 @@
 var app = getApp()
 var Bmob = require('../../utils/bmob.js'); 
+var wxDraw = require("../../utils/wxdraw.min.js").wxDraw;
+var Shape = require("../../utils/wxdraw.min.js").Shape;
+var AnimationFrame = require("../../utils/wxdraw.min.js").AnimationFrame;
 Page({
   data: {
+    width:'',
+    height:'',
+   display:'',
+   userInfo:'',
   },
   onLoad: function () {
-      
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          height: res.windowHeight,
+          width: res.windowWidth,
+        })
+      }
+    })
+    
+    var context = wx.createCanvasContext('fire')
+    that.wxCanvas = new wxDraw(context, 0, 0, 400, 500);
+    var img = [];
+    for (var i = 1; i < 16; i++) {
+      var xxposition = Math.floor(Math.random() * 400);
+      img[i] = new Shape('image', { x: xxposition, y: 10 + i * 10, w: 6, h: 6, file: "../../images/xx.png" }, 'fill', false)
+      that.wxCanvas.add(img[i]);
+        img[i].animate({opacity: 0,}, {
+          duration:3000
+        }).start(true)
+    };
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+              that.setData({
+                userInfo: res.userInfo,
+                display:'none'
+              })
+            }
+          })
+        }
+      }
+    })
+
+    
   },
 
 // Do something when show.
   onShow: function (options) {
-    //查询lover表和_User表,进行对比
-   
+
+  },
+
+  onUnload: function () {
+    this.wxCanvas.clear(); //推荐这个
+  },
+
+  onGotUserInfo: function (e) {
+    var that = this;
+    console.log(e.detail.userInfo)
+    that.setData({
+      userInfo: e.detail.userInfo,
+      display: 'none'
+    })
   },
 })
--- utils/util.js
+++ utils/util.js
@@ -1,21 +1,36 @@
-function formatTime(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
+function formatTime(time) {
+  if (typeof time !== 'number' || time < 0) {
+    return time
+  }
 
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
+  var hour = parseInt(time / 3600)
+  time = time % 3600
+  var minute = parseInt(time / 60)
+  time = time % 60
+  var second = time
 
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return ([hour, minute, second]).map(function (n) {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+  }).join(':')
 }
 
-function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+function formatLocation(longitude, latitude) {
+  if (typeof longitude === 'string' && typeof latitude === 'string') {
+    longitude = parseFloat(longitude)
+    latitude = parseFloat(latitude)
+  }
+
+  longitude = longitude.toFixed(2)
+  latitude = latitude.toFixed(2)
+
+  return {
+    longitude: longitude.toString().split('.'),
+    latitude: latitude.toString().split('.')
+  }
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatLocation: formatLocation
 }
