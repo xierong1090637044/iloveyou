6757218e880ab8f600ecb9921041cfb78e1894b7
xr <xr@wolvestribe.com>
2018-06-01 13:11:00 +0800

    add iloveyou  project

---
+++ pages/personcenter/personcenter.js
@@ -0,0 +1,15 @@
+var app = getApp()
+var Bmob = require('../../utils/bmob.js'); 
+Page({
+  data: {
+  },
+  onLoad: function () {
+      
+  },
+
+// Do something when show.
+  onShow: function (options) {
+    //查询lover表和_User表,进行对比
+   
+  },
+})
